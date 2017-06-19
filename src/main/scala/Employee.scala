6e40e00626228fc584289ab2cf33b068fbeb544d
Administrator <Pa$$w0rd>
2017-06-19 17:04:45 +0000

    Alex test

---
+++ src/main/scala/Employee.scala
@@ -0,0 +1,8 @@
+/**
+  * Created by Administrator on 19/06/2017.
+  */
+class Employee(id:String, name:String, isManager:Boolean) extends Person(id, name)
+{
+  // code here
+
+}
--- src/main/scala/Person.scala
+++ src/main/scala/Person.scala
@@ -1,7 +1,6 @@
 /**
   * Created by matt on 19/06/17.
   */
-abstract class Person {
-  val id: String
-  val name: String
+abstract class Person(var id:String, var name: String) {
+
 }
