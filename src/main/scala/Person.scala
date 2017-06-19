9def5f8eb87b14f8a70d6fb9fb64dbf2c4ffe508
RyanMal90 <ryanmal90@gmail.com>
2017-06-19 16:14:36 +0000

    Ryans Commit

--- src/main/scala/Person.scala
+++ src/main/scala/Person.scala
@@ -1,10 +1,12 @@
 /**
   * Created by matt on 19/06/17.
   */
-
-class Customer(val id: String, val name: String) extends Person {
-  var rewardPoints: Int = 0
+abstract class Person {
+  val id: String
+  val name: String
 }
+
+
 
 
 
