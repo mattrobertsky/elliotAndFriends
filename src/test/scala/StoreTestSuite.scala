0f10116fde2f00a47a3264be2c2baf9f2285383c
Matt Roberts <matt.roberts2@digital.hmrc.gov.uk>
2017-06-19 17:17:52 +0100

    fleshed out persons test

--- src/test/scala/StoreTestSuite.scala
+++ src/test/scala/StoreTestSuite.scala
@@ -8,12 +8,25 @@ class StoreTestSuite extends FunSuite {
 
   val store: Store = new Store
 
-  test("Store.readPersons: create some Persons from a file") {
+  test("store.readPersons: create some Persons from a file") {
     store.readPersons()
     assert(store.personMap.size == 10)
     assert(store.personMap.contains("personID"))
   }
 
+  test("store.createPerson: create one Customer using the constructor") {
+    val customer: Customer = store.createPerson("someId", "someName", "customer")
+    assert(customer.id == "someId")
+    assert(customer.name == "someName")
+    assert(customer.getClass.getName == "Customer")
+  }
+
+  test("store.createEmployee: create one Customer using the constructor") {
+    val employee: Employee = store.createPerson("someId", "someName", "employee")
+    assert(employee.id == "someId")
+    assert(employee.name == "someName")
+    assert(employee.getClass.getName == "Employee")
+  }
 //  import Main.countChange
 //  test("countChange: example given in instructions") {
 //    assert(countChange(4,List(1,2)) === 3)
